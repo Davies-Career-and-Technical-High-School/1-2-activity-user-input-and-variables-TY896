8e0b7e23bc8842742bbc60b51ee529aeb4d3f57b
TYLER BROOKS <tyler.brooks@daviestech.org>
2020-10-01 15:15:31 +0000

    everything

--- Question1.java
+++ Question1.java
@@ -1,11 +1,16 @@
 /* Activity 1.2 - Question 1 */
 
-
+ import java.util.Scanner;
 
 class Question1 {
     public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+    String snack:
+    System.out.println("what is your favorite snack");
+    snack = scan.nextLine();
+    System.out.println("the" + snack + " really good");
 
         /* Write your code here */
-
+    
     }
 }
