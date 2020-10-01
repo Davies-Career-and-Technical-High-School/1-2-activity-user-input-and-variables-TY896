8f941997e7d7bbf1d6d768617d6264b58299b76a
TYLER BROOKS <tyler.brooks@daviestech.org>
2020-10-01 15:32:28 +0000

    alot

--- Question2.java
+++ Question2.java
@@ -1,10 +1,20 @@
 /* Activity 1.2 - Question 2 */
-
+import java.util.Scanner;
 
 
 class Question2 {
     public static void main(String[] args) {
-
+   Scanner scan = new Scanner(System.in);
+    String name1;
+    String name2;
+    String name3;
+    System.out.println("give 3 names");
+    name1 = scan.nextLine();
+    name2 = scan.nextLine();
+    name3 = scan.nextLine();
+    System.out.println(  name1  + 
+    
+    " " + " " name2  + " "  + name3 );
         /* Write your code here */
 
     }
