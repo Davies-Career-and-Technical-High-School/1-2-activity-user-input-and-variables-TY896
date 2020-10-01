d23088eb199714d65f8869dafe4927fd55380623
TYLER BROOKS <tyler.brooks@daviestech.org>
2020-10-01 21:27:00 +0000

    lines 17 and 16

--- Question3.java
+++ Question3.java
@@ -8,13 +8,13 @@ import java.util.Scanner;
 class Question3 {
       public static void main(String[] args) {
      Scanner scan = new Scanner(System.in);
+     String name;
+      String Adjective;
      System.out.println(" hi there pal What is your name?");
+      name = scan.nextLine();
      System.out.println(" what are you?");
-        String name;
-        String Adjective;
-        name = scan.nextLine();
-        Adjective = scan.nextline();
-        System.out.println(  name + "is" + Adjective);
+        Adjective = scan.nextLine();
+        System.out.println(  name + " is " + Adjective);
 
 
     }
